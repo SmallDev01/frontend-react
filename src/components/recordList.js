8ad6b939e4880dee413c23715dc5a4740f0c56af
Adrian <acabanas@smalldev.es>
2022-02-23 12:56:00 +0100

    cambiado url de recor

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -25,7 +25,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`http://backendnodealvaro-env.eba-ngeycm8c.eu-west-3.elasticbeanstalk.com/record/`);
+      const response = await fetch(`http://backendnodealvaro-env.eba-ngeycm8c.eu-west-3.elasticbeanstalk.com/record`);
 
       if (!response.ok) {
         const message = `An error occured: ${response.statusText}`;
