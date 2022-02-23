fe8883c3d691755444806f53ed9f84f82df27c61
Adrian <acabanas@smalldev.es>
2022-02-23 17:47:38 +0100

    cambiando cositas vistosas

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -68,14 +68,14 @@ export default function RecordList() {
   // This following section will display the table with the records of individuals.
   return (
     <div>
-      <h3>Record List</h3>
+      <h3>Lista de emppleados</h3>
       <table className="table table-striped" style={{ marginTop: 20 }}>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Level</th>
-            <th>Action</th>
+            <th>Nombre</th>
+            <th>Posicion</th>
+            <th>Nivel</th>
+            <th>Tipo</th>
           </tr>
         </thead>
         <tbody>{recordList()}</tbody>
