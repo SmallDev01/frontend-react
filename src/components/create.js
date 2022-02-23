962845aa7a65ef580a7feeda4d5437366bfa63df
Adrian <acabanas@smalldev.es>
2022-02-23 12:29:24 +0100

    cambiadno api

--- src/components/create.js
+++ src/components/create.js
@@ -23,7 +23,7 @@ export default function Create() {
     // When a post request is sent to the create url, we'll add a new record to the database.
     const newPerson = { ...form };
 
-    await fetch("http://localhost:5000/record/add", {
+    await fetch("http://backendnodealvaro-env.eba-ngeycm8c.eu-west-3.elasticbeanstalk.com/record/add", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
